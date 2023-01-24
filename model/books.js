559fa5768aa11f241a16a7a5563d7712a0e0ec95
Natalia Rusu <nataliarusu.rn@gmail.com>
2023-01-24 17:04:03 +0000

    add model/books.js functionality

--- model/books.js
+++ model/books.js
@@ -2,7 +2,15 @@
 const db = require('../database/database.js')
 
 const select_all_books = db.prepare(/*sql*/ `
-    SELECT name FROM authors
+    SELECT 
+    books.id, 
+    books.name,
+    books.year,
+    genres.name AS genres_name,
+    authors.name AS author_name
+    FROM books
+    JOIN genres ON books.genres_id = genres.id
+    JOIN authors ON books.author_id = authors.id
 `);
 console.log('books')
 function listBooks(){
