d4371c6640d51c1f5a9da6fa26a361ef7f01b00e
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-01-25 15:27:05 +0000

    Added tests

---
+++ tests/three.test.js
@@ -0,0 +1,5 @@
+const test = require("node:test");
+const assert = require("node:assert");
+const { searchProducts } = require("../src/model/products.js");
+
+test("User can search by year of book release", () => {});
---
+++ tests/two.test.js
@@ -0,0 +1,5 @@
+const test = require("node:test");
+const assert = require("node:assert");
+const { searchProducts } = require("../src/model/products.js");
+
+test("User can add new book title to database", () => {});
